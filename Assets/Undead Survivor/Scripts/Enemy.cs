90b7034433eacbe70a1fb55f33ae35301e02b053
eldkfnrk <johongjin01@gmail.com>
2026-03-30 17:53:53 +0900

    Make enemy die.
    Make enemy die when enemy's hp is 0.
    Make knockback system when enemy hit by bullet.

--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:572fc4a3db5060d5a19af00f2b6f2c11ce7e5783878f36f4e8eb421e7010f5d8
-size 2322
+oid sha256:7bde943f54b0360553c42983cdb2805706bff29793331f6940d5ced3f17d35ab
+size 5048
